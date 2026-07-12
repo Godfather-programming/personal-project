21e19b2c8af8cd0e614fd8ccc2d9288d42569b9b
Abbas Heydari <abbasheydari.110afg@gmail.com>
2026-07-13 00:56:53 +0430

    this is the end

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
+  basePath: isProd ? "/personal-project" : "",
+  assetPrefix: isProd ? "/personal-project/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
